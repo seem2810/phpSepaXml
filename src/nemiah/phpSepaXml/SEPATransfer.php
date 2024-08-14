ab2dc75f5b305caf07f4cc39327f382015ace68b
Steve <s@s-mue.de>
2024-08-14 06:50:29 +0200

    Update SEPATransfer.php

--- src/nemiah/phpSepaXml/SEPATransfer.php
+++ src/nemiah/phpSepaXml/SEPATransfer.php
@@ -103,7 +103,7 @@ class SEPATransfer extends SEPAFile {
 			$PmtTpInf = $PmtInf->addChild('PmtTpInf');
 			$PmtTpInf->addChild('SvcLvl')->addChild('Cd', 'SEPA');
 			
-			$PmtTpInf = $PmtInf->addChild('ReqdExctnDt', '1999-01-01'); //OK
+			$PmtTpInf = $PmtInf->addChild('ReqdExctnDt', $sequence); //OK
 			
 			$this->debitor->XMLTransfer($PmtInf);
 
